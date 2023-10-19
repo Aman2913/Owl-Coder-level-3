01d7d3b6fe5ddd8f130373e3a60e79338804aee1
AMAN KUMAR VERMA <96662657+Aman2913@users.noreply.github.com>
2023-10-19 19:59:35 +0530

    Create Find two Distinct Prime Numbers with Given Product.cpp

---
+++ Find two Distinct Prime Numbers with Given Product.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int prime(int n)
+{
+    if(n==1 or n==0)
+    return 0;
+    for(int i=2;i<n/2;i++)
+    {
+        if(n%i==0)
+        return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int n,c=0;
+    cin>>n;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            if(prime(i))
+            {
+                cout<<i<<" ";
+                c++;
+            }
+        }
+    }
+    if(c==0) cout<<"-1";
+}
